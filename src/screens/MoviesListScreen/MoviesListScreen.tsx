232d0dd25f8073b23d21faef2b4468750c770eb6
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2022-05-21 19:21:45 -0300

    feature: adds support for firebase analytics and crashlytics

--- src/screens/MoviesListScreen/MoviesListScreen.tsx
+++ src/screens/MoviesListScreen/MoviesListScreen.tsx
@@ -1,8 +1,12 @@
 import Screen from 'components/Screen'
 import React from 'react'
 import { Text } from 'react-native'
+// import crashlytics from '@react-native-firebase/crashlytics'
 
 const MoviesListScreen: React.FC = () => {
+  // useEffect(() => {
+  //   crashlytics().crash()
+  // }, [])
   return (
     <Screen>
       <Text>Hello world</Text>
