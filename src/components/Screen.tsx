cf9e5db9c20cce69198a115c206dcab244c8e1c3
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2022-05-21 22:25:44 -0300

    feature: adds logo to splash screen

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,16 +1,20 @@
 import useColorScheme from 'hooks/useColorScheme'
 import { createThemedStyles, useThemedStyles } from 'hooks/useThemedStyles'
 import React from 'react'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, StyleProp, ViewStyle } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
 
-const Screen: React.FC = ({ children }) => {
+interface Props {
+  style?: StyleProp<ViewStyle>
+}
+
+const Screen: React.FC<Props> = ({ children, style }) => {
   const colors = useColorScheme()
   const styles = useThemedStyles(themedStyles)
 
   return (
     <LinearGradient style={styles.root} colors={colors.Gradient}>
-      <SafeAreaView style={styles.root}>{children}</SafeAreaView>
+      <SafeAreaView style={[styles.root, style]}>{children}</SafeAreaView>
     </LinearGradient>
   )
 }
--- src/screens/SplashScreen/SplashScreen.tsx
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -3,10 +3,14 @@ import Screen from 'components/Screen'
 import { MainRoutes, MainStackNavigationProps } from 'navigation/models/MainStackModels'
 import { TabRoutes } from 'navigation/models/TabModels'
 import React, { useEffect } from 'react'
-import { Text } from 'react-native'
+import { Image } from 'react-native'
+import logo from 'assets/img/logo.png'
+import { createThemedStyles, useThemedStyles } from 'hooks/useThemedStyles'
 
 const SplashScreen: React.FC = () => {
   const { navigate } = useNavigation<MainStackNavigationProps<MainRoutes.SplashScreen>>()
+
+  const styles = useThemedStyles(themedStyles)
 
   useEffect(() => {
     setTimeout(() => {
@@ -16,10 +20,24 @@ const SplashScreen: React.FC = () => {
   }, [])
 
   return (
-    <Screen>
-      <Text>Hello world</Text>
+    <Screen style={styles.root}>
+      <Image source={logo} style={styles.image} />
     </Screen>
   )
 }
+
+const themedStyles = createThemedStyles(() => ({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  image: {
+    width: 250,
+    height: 250,
+    resizeMode: 'contain'
+  }
+}))
 
 export default SplashScreen
