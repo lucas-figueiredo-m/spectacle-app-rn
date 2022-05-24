47740082ab3517d3739877b59b780313145f5506
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2022-05-24 00:48:49 -0300

    feature: adds params to AddNewMovieScreen

--- src/screens/AddNewMovieScreen/AddNewMovieScreen.tsx
+++ src/screens/AddNewMovieScreen/AddNewMovieScreen.tsx
@@ -1,8 +1,11 @@
+import { useRoute } from '@react-navigation/native'
 import Screen from 'components/Screen'
+import { MainRoutes, MainStackScreenProps } from 'navigation/models/MainStackModels'
 import React from 'react'
 import { Text } from 'react-native'
 
 const AddNewMovieScreen: React.FC = () => {
+  const { params } = useRoute<MainStackScreenProps<MainRoutes.AddNewMovieScreen>>()
   return (
     <Screen>
       <Text>Hello world</Text>
